b971ff4c5b0c29261ec001d8f93face4c06dca42
Quartz-42 <benjamin.baroche@free.fr>
2023-10-12 15:31:06 +0200

    Use ParamConverter

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,16 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
-    /**
-     * @Route("/", name="post_index")
-     */
+    #[Route('/', name: 'post_index')]
     public function index(PostRepository $postRepository): Response
     {
         return $this->render('post/index.html.twig', [
@@ -19,17 +18,9 @@ class PostController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/post/{slug}", name="post_show")
-     */
-    public function show($slug, PostRepository $postRepository): Response
+    #[Route('/post/{slug}', name: 'post_show')]
+    public function show(Post $post): Response
     {
-        $post = $postRepository->findOneBy(['slug' => $slug]);
-
-        if (!$post) {
-            throw $this->createNotFoundException("Aucun article retrouvé avec ce slug !");
-        }
-
         return $this->render('post/show.html.twig', [
             'post' => $post
         ]);
